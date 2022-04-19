6ef0899010b28ba1c76e0b2b4c5912b748fc12fd
lkc <myprose78@gmail.com>
2022-04-19 18:46:45 +0900

    Initial commit

---
+++ Basic/src/Variables.kt
@@ -0,0 +1,26 @@
+fun main() {
+    //정수 리터럴
+    println(100)
+    println(100000000000) //Java랑 달리 10억이 넘어도 L을 붙이지 않는다
+
+    println(100_100_330_111)
+
+    // 실수 리터럴
+    println(11.11)
+    println(22.22f)
+
+    // 문자 리터럴
+    println('A')
+    println("옹")
+
+    println("문자열")
+
+    // 불리언
+    println(true); println(false)
+
+    // Raw String """문자열"""
+
+    // Nothing 함수가 정상적으로 끝나지 않는다를 의미
+
+    // Unit void를 의미
+}
